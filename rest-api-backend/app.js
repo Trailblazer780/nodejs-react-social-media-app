260ac3bd76e6465486431ede6b3ec00fa008a2d4
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-11-17 16:13:12 -0400

    CRUD work on posts and status, acc creation, auth

--- rest-api-backend/app.js
+++ rest-api-backend/app.js
@@ -1,18 +1,67 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const feedRoutes = require('./routes/feed');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+const multer = require("multer");
+const ENV_VAR = require("./env");
+const MONGODB_URI = ENV_VAR.MONGODB_API_KEY;
 
 const app = express();
 
-app.use(bodyParser.json()); // application/json
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4());
+  },
 });
 
-app.use('/feed', feedRoutes);
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-app.listen(8080);
+app.use(bodyParser.json()); // application/json
+app.use(multer({ storage: storage, fileFilter: fileFilter }).single("image"));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
